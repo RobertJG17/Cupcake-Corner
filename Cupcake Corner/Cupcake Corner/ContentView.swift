43c7b2a40d03689d3e36dca250265d0d934c936f
RobertJG17 <robertjguerra@yahoo.com>
2020-11-27 00:51:56 -0800

    Initialized Project with Relevant Assets

---
+++ Cupcake Corner/Cupcake Corner/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Cupcake Corner
+//
+//  Created by Robert Guerra on 11/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {}
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
